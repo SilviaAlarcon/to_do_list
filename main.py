8d0db4a60086cbd37e7222906ab674d7bc20724a
Silvia Alarcón <silviaalarcon@MacBook-Air-de-Silvia.local>
2022-02-20 23:19:23 -0600

    Template inheritance

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from flask import Flask, request, make_response, redirect, render_template
 
 app = Flask(__name__)
 
-todos = ['TODO 1', 'TODO 2']
+todos = ['Programar', 'Programar más :D']
 
 @app.route('/')
 def index():
